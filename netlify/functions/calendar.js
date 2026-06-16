127584f66a0da596d07a199fd987358429e78c7e
MATTE <max@matteprojects.com>
2026-06-16 17:12:08 -0400

    Update calendar.js

--- netlify/functions/calendar.js
+++ netlify/functions/calendar.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      process.env.GOOGLE_PRIVATE_KEY.split('\\n').join('\n'),
       ['https://www.googleapis.com/auth/calendar.readonly']
     );
 
